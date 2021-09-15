fdb5244013baf4ed86f1226bae4b2736063aaff1
ESR <82104698+esrdapp@users.noreply.github.com>
2021-09-16 00:48:58 +0100

    Update signSomething.js

--- signSomething.js
+++ signSomething.js
@@ -2,11 +2,15 @@ const EthCrypto = require("eth-crypto");
 const signerIdentity = EthCrypto.createIdentity();
 const message = EthCrypto.hash.keccak256(
 [
-  { type: "uint256", value: "5" },
   { type: "string", value: "Banana" }
 ]
 );
 const signature = EthCrypto.sign(signerIdentity.privateKey, message);
-console.log(`message: ${message}`);
+const publicKey = EthCrypto.publicKeyByPrivateKey(signerIdentity.privateKey);
+
+
+console.log(`hashed message: ${message}`);
 console.log(`signature: ${signature}`);
-console.log(`signer public key: ${signerIdentity.address}`);
+console.log(`signer's private_key: ${signerIdentity.privateKey}`);
+console.log(`signer's public_key: ${publicKey}`);
+console.log(`signer's wallet_address: ${signerIdentity.address}`);
